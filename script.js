49bbe11ddc1b00cb382a8e582c0d0b6a24085684
Alina <example@gmail.com>
2021-01-06 16:38:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// console.log(1);
+
+// let b = alert("something");
+
+// console.log(4/0);
+// const num = Number.MAX_SAFE_INTEGER;
+// console.log(num);
+
+// console.log(num + 4);
+// const answer = +prompt("enter", "18");
+// console.log(typeof(answer));
+// const answers = [];
+
+// answers[0] = prompt("Напишите имя");
+// answers[1] = prompt("Напишите фамилию");
+// answers[2] = prompt("Напишите город");
+
+// console.log(typeof(answers));
+let a = 10;
+let b = 10;
+
+a++;
+b--;
+console.log(a);
+console.log(b);
